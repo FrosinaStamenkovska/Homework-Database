f8c15436493b2c629f992ccc86a3fdce781c630a
FrosinaStamenkovska <117694114+FrosinaStamenkovska@users.noreply.github.com>
2023-06-06 03:05:28 +0200

    Update Workshop02_02.sql

--- Homework5/Workshop02_02.sql
+++ Homework5/Workshop02_02.sql
@@ -1,9 +1,8 @@
 --WORKSHOP 02.2
---• Create new procedure called CreateGradeDetail
---• Procedure should add details for specific Grade (new record for new AchievementTypeID,
---Points, MaxPoints, Date for specific Grade)
---• Output from this procedure should be resultset with SUM of GradePoints calculated with
---formula AchievementPoints/AchievementMaxPoints*ParticipationRate for specific Grade
+-- Create new procedure called CreateGradeDetail
+-- Procedure should add details for specific Grade (new record for new AchievementTypeID, Points, MaxPoints, Date for specific Grade)
+-- Output from this procedure should be resultset with SUM of GradePoints calculated with formula:
+-- AchievementPoints/AchievementMaxPoints*ParticipationRate for specific Grade
 
 CREATE PROCEDURE dbo.CreateGradeDetail
 (
@@ -14,18 +13,23 @@ CREATE PROCEDURE dbo.CreateGradeDetail
 )
 AS
 BEGIN
-		DECLARE @AchievementDate date,
-				@GradePoints int
+	INSERT INTO dbo.GradeDetails
+	(GradeID,
+	AchievementTypeID,
+	AchievementPoints,
+	AchievementMaxPoints,
+	AchievementDate)
+	VALUES
+	(@GradeId, 
+	@AchievementTypeId,
+	@AchievementPoints,
+	@AchievementMaxPoints,
+	GETDATE())
 
-		Select @AchievementDate = g.CreatedDate
-		From dbo.Grade g
-		Where g.Id = @GradeId
-
-		INSERT INTO dbo.GradeDetails(GradeID, AchievementTypeID, AchievementPoints, AchievementMaxPoints, AchievementDate)
-		VALUES(@GradeId, @AchievementTypeId, @AchievementPoints, @AchievementMaxPoints, @AchievementDate)
-
-		Select @GradePoints = SUM(@AchievementPoints / @AchievementMaxPoints * a.ParticipationRate)
-		From dbo.AchievementType a
-		Where a.ID = @AchievementTypeId
+	Select SUM (CAST(g.AchievementPoints AS DECIMAL(18,2)) / CAST (g.AchievementMaxPoints AS DECIMAL(18,2)) * ac.ParticipationRate) AS TotalPoints
+	From dbo.GradeDetails g
+	INNER JOIN dbo.AchievementType ac ON ac.Id = g.AchievementTypeID
+	Where g.GradeID = @GradeId 
+	Group By g.GradeID	
 
 END
